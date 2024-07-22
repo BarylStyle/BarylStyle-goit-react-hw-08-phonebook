cc86c0ce703080174d91186f5e14f12166c3eabe
BarylStyle <mateusz.consulting@gmail.com>
2024-07-22 17:53:50 +0200

    update

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const PrivateRoute = ({ element: Component, ...rest }) => {
-  const isAuthenticated = useSelector(state => !!state.auth.token); // Sprawdzenie, czy token jest obecny
+  const isAuthenticated = useSelector(state => !!state.auth.token); 
 
   return (
     <Route
